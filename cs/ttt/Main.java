e1c7d61be98484f8a120121a3e8c4fec60acaded
firefly431 <firefly431.awesome@gmail.com>
2014-11-12 12:47:07 -0600

    Remove old code

--- cs/ttt/Main.java
+++ cs/ttt/Main.java
@@ -16,35 +16,6 @@ import javax.swing.JPanel;
  */
 public class Main {
     public static void main(String argv[]) {
-//        Board gameBoard = new Board();
-//        Board.Symbol currentPlayer = Board.Symbol.X;
-//        Scanner s = new Scanner(System.in);
-//        while (!gameBoard.isTerminal()) {
-//            gameBoard.print();
-//            System.out.println();
-//            System.out.println("Scores:");
-//            int maxScore = currentPlayer.max() ? Integer.MIN_VALUE : Integer.MAX_VALUE;
-//            ArrayList<Integer> maxMoves = new ArrayList<Integer>();
-//            for (int i : gameBoard.getMovePositions()) {
-//                Board consider = gameBoard.move(i, currentPlayer);
-//                int score = consider.alphaBeta(currentPlayer.other());
-//                System.out.println(i + ": " + score);
-//                if (currentPlayer.max() && score > maxScore || currentPlayer.min() && score < maxScore) {
-//                    maxMoves.clear();
-//                    maxScore = score;
-//                }
-//                if (score == maxScore) {
-//                    maxMoves.add(i);
-//                }
-//            }
-//            System.out.println("It is " + currentPlayer.getSymbol() + "'s turn");
-//            System.out.println("We are " + (currentPlayer.max() ? "maximizing" : "minimizing"));
-//            System.out.print("Best score: " + maxScore);
-//            System.out.println(", moves: " + maxMoves);
-//            int move = s.nextInt();
-//            gameBoard = gameBoard.move(move, currentPlayer);
-//            currentPlayer = currentPlayer.other();
-//        }
         BoardPanel bp = new BoardPanel();
         bp.setSymbol(Board.Symbol.O);
         bp.setProvider(new AlphaBetaWeightProvider());
